60d45fd78a3da5fd968d85a003151c5b854e40d7
OLDLi <wenju1991@163.com>
2021-04-08 07:46:43 +0800

    feat：添加自定义index.html功能

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
+const { resolve } = require('path')
+const { copyFileSync } = require('fs')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import handlebars from 'vite-plugin-handlebars'
 import loadModule from './plugins/load-module'
 import loadCode from './plugins/load-code'
 
-// https://vitejs.dev/config/
+//复制入口文件index.html，用户可通过这种方式来使用自己的index.html
+copyFileSync('./entries/index.html', './index.html')
+
 export default defineConfig({
-  plugins: [loadModule(), loadCode(), vue()],
+  plugins: [
+    loadModule(),
+    loadCode(),
+    vue(),
+    //index.html文件处理插件(https://github.com/alexlafroscia/vite-plugin-handlebars)
+    handlebars({
+      context: {
+        /** 版权信息 */
+        copyright: '版权所有：OLDLI',
+        /** Logo */
+        logo: './images/logo.png',
+      },
+    }),
+  ],
   server: {
     port: 6220,
+  },
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index1111.html'),
+    },
   },
 })
