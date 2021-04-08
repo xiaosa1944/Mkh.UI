aab2a22b75869ef1676e9bdf12101a398f2a19f6
OLDLi <wenju1991@163.com>
2021-04-08 11:45:18 +0800

    feat:新增index.html模板替换以及代码压缩插件

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,28 @@
-const { resolve } = require('path')
 const { copyFileSync } = require('fs')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import handlebars from 'vite-plugin-handlebars'
 import loadModule from './plugins/load-module'
 import loadCode from './plugins/load-code'
+import htmlTransform from './plugins/html-transform'
 
 //复制入口文件index.html，用户可通过这种方式来使用自己的index.html
 copyFileSync('./entries/index.html', './index.html')
 
 export default defineConfig({
   plugins: [
-    loadModule(),
-    loadCode(),
-    vue(),
-    //index.html文件处理插件(https://github.com/alexlafroscia/vite-plugin-handlebars)
-    handlebars({
-      context: {
+    htmlTransform({
+      render: {
         /** 版权信息 */
         copyright: '版权所有：OLDLI',
         /** Logo */
         logo: './images/logo.png',
       },
     }),
+    loadModule(),
+    loadCode(),
+    vue(),
   ],
   server: {
     port: 6220,
-  },
-  build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'index1111.html'),
-    },
   },
 })
